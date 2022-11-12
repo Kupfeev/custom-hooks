48e6017e3347b43b7ca69c593b333030004f7184
Enzo <tassienzo@gmail.com>
2022-11-11 23:44:48 -0300

    Added documentation in useObserver hook

--- useObserver/useObserver.js
+++ useObserver/useObserver.js
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 
+
 export const useObserver = ( elements ) => {
+  // ! TO USE THIS HOOK, ARGUMENT "elements" MUST CONTAIN THE PROPERTY "current" AND IT HAS TO BE AN ARRAY, even if there is only one value. Follow the guideline below.
+  // ? const foo = useRef([]);
+  // ? <HTMLelement ref={ el => foo.current[0] = el } />
 
     useEffect(() => {
         const cardsObserver = new IntersectionObserver((entries) => {
